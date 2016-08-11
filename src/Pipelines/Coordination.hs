de631cfaf480fa815382fdec1ee13e8d27414889
Eric Conlon <ejconlon@gmail.com>
2016-08-11 09:35:18 -0700

    monadwatch

--- src/Pipelines/Coordination.hs
+++ src/Pipelines/Coordination.hs
@@ -7,19 +7,17 @@ import Control.Monad.Base
 import Control.Monad.IO.Class
 import Control.Monad.Reader
 import qualified Data.Map as M
+import List.Transformer
 import Pipelines.Core
-import qualified System.FSNotify as N
+import Pipelines.Filesystem
 
 data CoordinationEnv = CoordinationEnv
   { _coordinationEnvBaseDir :: FilePath
-  , _coordinationEnvPlans   :: M.Map String Plan
+  , _coordinationEnvPlans   :: M.Map Name Plan
   } deriving (Show, Eq)
 
-type MonadCoordination b m = (Monad b, MonadIO b, Monad m, MonadIO m, MonadBase b m,
+type MonadCoordination b m = (Monad b, MonadWatch b, MonadFS b, Monad m, MonadBase b m,
                               MonadReader CoordinationEnv m)
 
 initializeDirs :: MonadCoordination b m => m ()
 initializeDirs = undefined
-
-watchDirs :: MonadCoordination b m => m ()
-watchDirs = undefined
--- src/Pipelines/Filesystem.hs
+++ src/Pipelines/Filesystem.hs
@@ -1,7 +1,11 @@
 module Pipelines.Filesystem where
 
+import Control.Concurrent.Chan
 import qualified Data.ByteString.Lazy as BL
+import Data.Time.Clock
+import List.Transformer
 import System.Directory
+import qualified System.FSNotify as N
 
 class MonadFS b where
   readFileFS :: FilePath -> b BL.ByteString
@@ -14,3 +18,43 @@ instance MonadFS IO where
   writeFileFS = BL.writeFile
   doesFileExistFS = doesFileExist
   doesDirectoryExistFS = doesDirectoryExist
+
+data WatchEventType = AddedWatchEvent | ModifiedWatchEvent | RemovedWatchEvent deriving (Show, Eq)
+
+data WatchEvent = WatchEvent
+  { _watchEventType :: WatchEventType
+  , _watchEventPath :: FilePath
+  , _watchEventTime :: UTCTime                     
+  } deriving (Show, Eq)
+
+data Watch b c = Watch
+  { _watchEvents  :: ListT b (WatchEvent, c)
+  , _watchStop    :: b ()
+  }
+
+eventToWatchEvent :: N.Event -> WatchEvent
+eventToWatchEvent (N.Added p t) = WatchEvent AddedWatchEvent p t
+eventToWatchEvent (N.Modified p t) = WatchEvent ModifiedWatchEvent p t
+eventToWatchEvent (N.Removed p t) = WatchEvent RemovedWatchEvent p t
+
+watchEventToEvent :: WatchEvent -> N.Event
+watchEventToEvent (WatchEvent ty p t) =
+  case ty of
+    AddedWatchEvent -> N.Added p t
+    ModifiedWatchEvent -> N.Modified p t
+    RemovedWatchEvent -> N.Removed p t
+
+readChanToList :: Chan a -> ListT IO a
+readChanToList c = ListT $ do
+  value <- readChan c
+  return $ Cons value $ readChanToList c
+
+class MonadWatch b where
+  watchDir :: FilePath -> (WatchEvent -> Bool) -> c -> b (Watch b c)
+
+instance MonadWatch IO where
+  watchDir path pred ctx = N.withManager $ \manager -> do
+    chan <- newChan
+    stop <- N.watchDirChan manager path (\e -> pred (eventToWatchEvent e)) chan
+    let list = (\e -> (eventToWatchEvent e, ctx)) <$> readChanToList chan
+    return $ Watch list stop
